374b1a0155b90b7ee3226ad3972ce1b743679490
Gui ximiti <gui.m.schmidt@gmail.com>
2023-01-09 20:29:51 -0300

    Day 1 of 50

---
+++ Javascript-Basico/26/Mais sobre Numbers.js
@@ -0,0 +1,20 @@
+
+
+let num1 = 15.012412412;
+let num2 = 2.5;
+
+//console.log(num1.toString() + num2); //resultado é 12.5 porque concatena e não soma, nesse caso o num1 vira uma string
+
+//console.log(num1.toString(2)); Colocando o 2 ele  retorna o numero do binario da string. IMPORTANTE
+
+//console.log(num1.toFixed(2)) Fixa o numero de casas apos a virgula, neste caso são 2.
+
+//console.log(Number.isInteger(num1)); Retorna true or false se o numero é um inteiro.
+
+let temp = num1 * 'Ola'; // == NaN
+
+// console.log(Number.isNaN(temp)); Retorna se isso não é um numero ou uma conta de verdade. Retorna True or false.
+
+
+
+
---
+++ Javascript-Basico/27/Objeto-Math.js
@@ -0,0 +1,9 @@
+
+
+let num1 = 9.56784;
+// let num2 = Math.floor(num1); Arrendonda para baixo 
+// let num2 = Math.ceil(num1); Arrendonda para cima
+// let num2 = Math.round(num1); Arredonda para o numero mais proximo 
+//console.log(Math.max(1,2,3,4,5121,21541,43,5326563,232,53452)) Retorna o maior numero dessa sequencia.
+//console.log(Math.min(1,2,3,4,5121,21541,43,5326563,232,53452)) Retorna o maior numero dessa sequencia.
+//const aleatorio = Math.random() * (10 - 5) + 5; Retorna um numero aleatorio entre 10 e 5 (Posso mudar os limites)

87330e90b7d2b6c8253857c95d051623ebdda830
Gui ximiti <gui.m.schmidt@gmail.com>
2023-01-12 20:27:53 -0300

    Day 4 of 50

---
+++ Javascript-Basico/34/script.js
@@ -0,0 +1,41 @@
+
+
+function meuEscopo(){
+
+    const form = document.querySelector(".form");
+    const resultado = document.querySelector(".resultado")
+    const pessoas = [];
+
+    
+
+     //Função que previne que o formulario seja enviado e atualizado.
+
+    function recebeEventoForm(evento){
+        evento.preventDefault();
+
+
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoas.push({
+            nome:nome.value,
+            sobrenome:sobrenome.value,
+            peso:peso.value,
+            altura:altura.value
+        })
+        resultado.innerHTML+= `nome: ${nome.value} sobrenome: ${sobrenome.value} peso: ${peso.value} altura: ${altura.value}`
+        console.log(pessoas)
+    }
+    
+     form.addEventListener('submit', recebeEventoForm)
+
+}
+
+
+
+
+
+meuEscopo();
+
---
+++ Javascript-Basico/35/OperadoresComparacao.js
@@ -0,0 +1,12 @@
+
+
+
+// > maior que
+// >= maior que ou igual a
+// < menor que
+// <= menor que ou igual a
+// == igualdade (Checa valor)
+// === igualdade estrita (Checa valor e tipo)
+// != diferente (Checa valor)
+// !=== diferente estrito (Checa valor e tipo)
+
